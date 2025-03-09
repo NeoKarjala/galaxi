c8a09153e5e67e0f51bd76fc9edc6caaa9eb4c79
ryypmi <mikko.ryyppo@hotmail.fi>
2025-03-09 18:52:04 +0200

    Tyyli muutoksia tehty sekä kieli ja kirjaudu ulos napit lisätty

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,21 +1,39 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <>
-      <div className='bg-secondary text-primary flex px-5 py-32 w-72'>
-        <ul className='menu flex gap-4 w-full'>
-          <li>
-            <Link className='btn btn-outline' to='/'>
-              Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link className='btn btn-outline' to='/ownReservations'>
-              Omat varaukset
-            </Link>
-          </li>
-        </ul>
+      <div className="bg-secondary text-secondary flex flex-col justify-between px-8 py-32 w-80">
+        <div>
+          <ul className="menu flex gap-4 w-full">
+            <li>
+              <Link
+                className="btn btn-outline bg-primary hover:border-primary hover:text-primary"
+                to="/"
+              >
+                Kalenteri
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="btn btn-outline bg-primary hover:border-primary hover:text-primary"
+                to="/ownReservations"
+              >
+                Omat varaukset
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul className="menu flex gap-4 w-full">
+            <li className="btn btn-outline bg-primary hover:border-primary hover:text-primary">
+              Kieli / Language
+            </li>
+            <li className="btn btn-outline bg-primary hover:border-primary hover:text-primary">
+              Kirjaudu ulos
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
